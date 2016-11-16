35f75e989aad5c01e1f0480c65fbd674f41c0885
Jason Brown <jason.brown08@mymona.uwi.edu>
2016-11-16 19:22:53 +0000

    added js and css files

--- world.php
+++ world.php
@@ -4,12 +4,20 @@ $host = getenv('IP');
 $username = getenv('C9_USER');
 $password = '';
 $dbname = 'world';
+$country= $_GET['country'];//Get country to search for
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
 $stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if($country == 'all')
+{
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+else
+{
+    $results = $stmt->query('SELECT * FROM  countries WHERE name LIKE "%$country%"');
+}
 
 echo '<ul>';
 foreach ($results as $row) {
