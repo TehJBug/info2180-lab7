345cb49ee75591707aad8317ebcc2a7184c24b56
Jason Brown <jason.brown08@mymona.uwi.edu>
2016-11-18 01:21:36 +0000

    done

--- world.js
+++ world.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
     
     $("#lookup").click(function(){
+        var allVal;
+        
+        if($("#all").is(':checked'))
+        {
+            allVal = 'true';
+        }
+        else
+        {
+            allVal='false';
+        }
+        
+        //alert(allVal);
        
-       $.get('world.php',{country:$("#country").val() , all:false},function(data){
-                        
-                        //alert(data);
-                        $('#result').html(data);
-                        
-                    });
+       $.get('world.php',{country:$("#country").val() , all:allVal},function(data){
+            //alert(data);
+            $('#result').html(data);
+        });
         
     });
     
